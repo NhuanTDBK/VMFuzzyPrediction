650aa12c7068da3b598b8013b238192464d1916f
Tran Duc Nhuan <tranducnhuan1994@gmail.com>
2016-10-07 21:16:52 +0700

    Git fixed Experiment BPNN inverse transform

--- experiments/ExperimentBPNNM.py
+++ experiments/ExperimentBPNNM.py
@@ -1,6 +1,6 @@
 from __init__fuzzy import *
 
-dat = pd.read_csv('sampling_617685_metric_10min_datetime.csv',parse_dates=True,index_col=0)[:3000]
+dat = pd.read_csv('sampling_617685_metric_10min_datetime_origin.csv',parse_dates=True,index_col=0)[:3000]
 gFeeder = GFeeder()
 metrics = ['cpu_rate','mem_usage','disk_space']
 def experiment(sliding_number):
@@ -11,11 +11,11 @@ def experiment(sliding_number):
     y_cpu = ypred[:,0]
     y_ram = ypred[:,1]
     y_disk_space = ypred[:,2]
-    score_mae_CPU = mean_absolute_error(y_cpu, y_test[:,0])
-    score_mae_RAM =  mean_absolute_error(y_ram, y_test[:,1])
-    score_mae_diskio = mean_absolute_error(y_disk_space,y_test[:,2])
+    score_mae_CPU = mean_absolute_error(cpu_scaler.inverse_transform(y_cpu), cpu_scaler.inverse_transform(y_test[:,0]))
+    score_mae_RAM =  mean_absolute_error(ram_scaler.inverse_transform(y_ram), ram_scaler.inverse_transform(y_test[:,1]))
+    score_mae_diskio = mean_absolute_error(diskio_scaler.inverse_transform(y_disk_space),diskio_scaler.inverse_transform(y_test[:,2]))
     np.savez('model_saved/BPNNM_%s_%s'%(sliding_number,score_mae_CPU),y_pred=ypred, y_true=y_test)
-    return sliding_number, score_mae_CPU, score_mae_RAM, score_mae_diskio
+    return sliding_number, score_mae_CPU, score_mae_RAM, score_mae_diskio,score_mae_diskio
 
 result = [[experiment(sliding_number=i) for i in np.arange(2,6)] for j in np.arange(10)]
 cols = ["sliding_number"]
